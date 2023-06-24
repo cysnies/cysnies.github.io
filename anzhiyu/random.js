a7f53d4c776bcfdc3968f197e68489a88c2e8687
cysnies <cysnies@gmail.com>
2023-06-24 09:54:58 +0800

    Site updated: 2023-06-24 09:54:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/23/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/06/24/Typora/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
